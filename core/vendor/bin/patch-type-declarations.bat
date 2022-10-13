3e1271ebfe9a1d4db3eb56bb44191c5c9a8a47a8
miarta <miarta.igede@gmail.com>
2022-10-13 21:34:03 +0800

    db

--- core/vendor/bin/patch-type-declarations.bat
+++ core/vendor/bin/patch-type-declarations.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/patch-type-declarations
+SET BIN_TARGET=%~dp0/../symfony/error-handler/Resources/bin/patch-type-declarations
 php "%BIN_TARGET%" %*
