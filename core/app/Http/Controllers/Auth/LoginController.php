155cf66e74fab21dbc91eb3d97e8250503beb060
Adi Perdana <dewaadiperdana@gmail.com>
2022-10-26 20:20:38 +0800

    remove debug code

--- core/app/Http/Controllers/Auth/LoginController.php
+++ core/app/Http/Controllers/Auth/LoginController.php
@@ -64,7 +64,6 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        dd($request->all());
         $this->validateLogin($request);
 
         if(isset($request->captcha)){
