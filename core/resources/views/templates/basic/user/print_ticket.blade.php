d0dd62e686484cccf260b9cba9b3d5f83bb19a31
Risfandi Bayu <risfandibayu19@gmail.com>
2022-12-20 19:47:07 +0700

    update favicon print ticker

--- core/resources/views/templates/basic/user/print_ticket.blade.php
+++ core/resources/views/templates/basic/user/print_ticket.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="{{ getImage('assets/images/logoIcon/favicon.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ getImage('assets/images/favicon-new.png') }}">
+    {{-- <link rel="icon" type="image/png" href="{{ asset('assets/images/favicon-new.png') }}"> --}}
     <title>{{ $general->sitename($pageTitle) }}</title>
     <style>
         @media screen,
