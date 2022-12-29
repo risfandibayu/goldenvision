8a5c7f0374093234374a186505669d82d8baedb5
miarta <miarta.igede@gmail.com>
2022-12-29 19:11:19 +0800

    all user

--- core/app/Http/Controllers/UserController.php
+++ core/app/Http/Controllers/UserController.php
@@ -102,7 +102,7 @@ class UserController extends Controller
      public function allInUsers(){
         $page_title = 'All Users';
         $empty_message = 'No user found';
-        $users = User::all()->latest()->paginate(getPaginate());
+        $users = User::latest()->paginate(getPaginate());
         return view('admin.users.users-list', compact('page_title', 'empty_message', 'users'));
     }
 
