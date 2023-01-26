0c1a2230decab61b2a86b17b1a26ad8f9a5dc28f
miarta <miarta.igede@gmail.com>
2023-01-26 15:08:42 +0800

    update cron is deliver

--- core/app/Http/Controllers/CronController.php
+++ core/app/Http/Controllers/CronController.php
@@ -555,7 +555,7 @@ class CronController extends Controller
                     'left_lv'   => $kiri
                 ]);
                 $true += 1;
-            }else if($kiri > 3 && $kanan > 3){
+            }else if($kiri > 3 || $kanan > 3){
                 $userex->update([
                     'bonus_deliver'     => 1,
                     'right_lv'          => $kanan,
