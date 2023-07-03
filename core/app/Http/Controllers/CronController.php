76976eb1e3fc8bf213c36a6256c94ab0aea0a823
miartayasa <miartayasa10@gmail.com>
2023-07-04 02:29:00 +0800

    cron gold fixed

--- core/app/Http/Controllers/CronController.php
+++ core/app/Http/Controllers/CronController.php
@@ -689,7 +689,7 @@ class CronController extends Controller
                 }
                 $userex = UserExtra::where('user_id',$userID)->first();
                 if($userex){
-                    if($kiri == 3 && $kanan == 3){
+                    if($kiri >= 3 && $kanan >= 3){
                         $userex->update([
                             'is_gold'   => 1,
                             'right_lv'  => $kanan,
@@ -706,7 +706,7 @@ class CronController extends Controller
                         ]);
                         $true += 1;
                     }
-                    if($p_kiri == 3 && $p_kanan == 3){
+                    if($p_kiri >= 3 && $p_kanan >= 3){
                         $userex->update([
                             'is_gold'   => 1,
                             'right_lv'  => $kanan,
