5bb96f06a12753b49b5c36d35c57eb9296f4827d
miartayasa <miartayasa10@gmail.com>
2023-08-17 21:03:10 +0800

    demo akun

--- core/resources/views/templates/basic/user/dashboard/gems.blade.php
+++ core/resources/views/templates/basic/user/dashboard/gems.blade.php
@@ -62,8 +62,8 @@
                     </form>
                 @endif
                 <br>
-                <form action="{{ route('user.login.ayamku') }}" method="post" target="_blank">
-                    <input type="hidden" name="username" id="" value="bozgemsgit">
+                <form action="{{ route('user.login.ayamku') }}" method="get" target="_blank">
+                    <input type="hidden" name="username" id="" value="brogems">
                     <button class="btn btn-warning btn-block" type="submit">
                         <i class="menu-icon las la-sign-in-alt"></i>Share Demo Account</button>
                 </form>
