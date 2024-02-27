87d25a210e008106be7c6449af832ae2fabc8f1d
I Gede Miarta Yasa <miartayasa10@gmail.com>
2024-02-27 10:48:01 +0800

    add new db

--- _DB/backup_db_script.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-# Mendapatkan tanggal saat ini dalam format YYYYMMDD
-current_date=$(date +"%Y%m%d")
-
-# Nama file backup dengan tanggal
-backup_filename="masterplan_main_db_backup/masterplan_main_$current_date.sql"
-
-mysqldump -u masterpl_masterpl_dev -p xlnH7Br2PeI$ masterpl_main member_grows rekenings transactions urewards users user_extras user_golds user_pin withdrawals > "$backup_filename"
