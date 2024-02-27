5670e7a41b206738cdbf97c66f0b71a6535584cc
Risfandi Bayu <risfandibayu19@gmail.com>
2024-02-27 21:13:40 +0700

    update

--- core/app/Http/Controllers/NewCronController.php
+++ core/app/Http/Controllers/NewCronController.php
@@ -32,7 +32,7 @@ class NewCronController extends Controller
 
             $us = User::where('id', $uex->user_id)->first();
 
-            if (Date('H') != "21") {
+            if (Date('H') != "00") {
                 continue;
             }
 
